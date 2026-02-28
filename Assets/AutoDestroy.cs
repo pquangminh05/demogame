04118f4fcadee1872f5eb01bdcc52e395e3dd3f2
pquangminh05 <phamminhheo2k5@gmail.com>
2026-02-28 20:23:45 +0700

    demopart5

--- Assets/AutoDestroy.cs
+++ Assets/AutoDestroy.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class AutoDestroy : MonoBehaviour
 {
     public float lifeTime = 0.5f;
-
+//demo part5
     void Start()
     {
         Destroy(gameObject, lifeTime);
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-// Update is called once per frame
+// demopart5
 void Update()
     {
         var worldPoint = 
